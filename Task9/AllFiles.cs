c98ea35a4162378a716b16bb6079de6a49d0e80c
RezeR1320 <Artem@DESKTOP-J9AHB8E>
2020-10-21 03:13:24 +0300

    Добавьте файлы проекта.

---
+++ Task9/AllFiles.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Task9
+{
+    class AllFiles
+    {
+        private DirectoryInfo dirInfo;
+        private string path;
+        public string Path
+        { 
+            get { return path; } 
+            set { path = value; dirInfo = new DirectoryInfo(path); } 
+        }
+        public AllFiles(string p)
+        {
+            Path = p;
+            dirInfo = new DirectoryInfo(path);
+        }
+        public void DisplayFiles()
+        {
+            int index = 0;
+            foreach(FileInfo s in dirInfo.GetFiles())
+                Console.WriteLine(index+++": "+s.Name);
+        }
+        public void ReadFile(int index)
+        {
+            string[] files = Directory.GetFiles(path);
+            if(index >= 0 && index < files.Length)
+            {
+                using (FileStream fl = File.OpenRead(files[index]))
+                {
+                    byte[] arr = new byte[fl.Length];
+                    fl.Read(arr, 0, arr.Length);
+                    string text = Encoding.Default.GetString(arr);
+                    Console.WriteLine("\ntext:\n\n" + text);
+                }
+            }
+            
+        }
+    }
+}
---
+++ Task9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AllFiles f = new AllFiles(@"D:\directory");
+            f.DisplayFiles();
+            f.ReadFile(0);
+        }
+    }
+}
